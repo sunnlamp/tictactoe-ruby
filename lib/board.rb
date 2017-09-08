4b5c532953e57c896df75704730c862ac66344a6
Rogelio Zamudio <yeyozaz@gmail.com>
2017-09-07 21:31:48 -0500

    modified file names due to responsibilities

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,11 @@
-#!/usr/bin/env ruby
+class Board
+  def board
+    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+
+  def print_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,12 +1,12 @@
-require 'game'
+require 'board'
 
-describe Game
+describe Board
 
   describe "the tic tac toe board" do
 
     # Game uses an array to facilitate the board
     it "initializes the game board" do
-      game = Game.new
+      game = Board.new
       game.board
       expect(game.board).to be_instance_of(Array)
     end
@@ -14,19 +14,19 @@ describe Game
     # Game uses numbers to identify where the player will place their character
     it "returns an array of numbers" do
       board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-      game = Game.new
+      game = Board.new
       game.board
       expect(game.board).to eq(board)
     end
 
     # Game uses print_board to print the game board after every move
     it "prints the board" do
-      game = Game.new
+      game = Board.new
       expect(puts " 1 | 2 | 3 \n 4 | 5 | 6 \n 7 | 8 | 9 ").to match(game.print_board(game.board))
     end
 
     it "prints the game to stdout" do
-      game = Game.new
+      game = Board.new
       expect { game.print_board(game.board) }.to output.to_stdout
     end
   end
