ea19da59e2fffd588568c08fe791e8806e22d30b
Rogelio Zamudio <yeyozaz@gmail.com>
2017-09-21 13:28:54 -0500

    added attr_reader to Board, modified tests accordingly

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,16 @@
 class Board
-  def board
+  attr_reader :board
+  def initialize
     @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
-  def print_board(board)
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  def print_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def make_x_move(position)
+    @board.delete_at(position)
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,7 +7,7 @@ describe Board
     # Game uses an array to facilitate the board
     it "initializes the game board" do
       game = Board.new
-      game.board
+
       expect(game.board).to be_instance_of(Array)
     end
 
@@ -22,11 +22,18 @@ describe Board
     # Game uses print_board to print the game board after every move
     it "prints the board" do
       game = Board.new
-      expect(puts " 1 | 2 | 3 \n 4 | 5 | 6 \n 7 | 8 | 9 ").to match(game.print_board(game.board))
+      expect(puts " 1 | 2 | 3 \n 4 | 5 | 6 \n 7 | 8 | 9 ").to match(game.print_board)
     end
 
     it "prints the game to stdout" do
       game = Board.new
-      expect { game.print_board(game.board) }.to output.to_stdout
+      expect { game.print_board }.to output.to_stdout
+    end
+
+    it "replaces the original cell value with an X" do
+      game = Board.new
+      game.make_x_move(0)
+      board = ['X', 2, 3, 4, 5, 6, 7, 8, 9]
+      expect(game.board).to eq(board)
     end
   end
