8189f50b6d06c2a06844d2b83b5a60ecaf14e6b8
Rogelio Zamudio <yeyozaz@gmail.com>
2017-09-24 14:04:25 -0500

    added check diagonal method, comments for what methods do

--- lib/board.rb
+++ lib/board.rb
@@ -1,21 +1,30 @@
 class Board
   attr_reader :board
+
+  # initializes an instance of the board
   def initialize
     @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
+  # used to print the board
   def print_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
+  # changes the value of the array index to the players mark
   def make_move(position, player)
     @board.delete_at(position)
     @board.insert(position, player)
   end
 
+  # used to check the two diagonal winning moves
   def check_diagonals(position, player)
-    @board.select { |a| a == player}
+    if @board[0] == player && @board[4] == player && @board[8] == player
+      return :winner
+    elsif @board[2] == player && @board[4] == player && @board[6] == player
+      retunr :winner
+    end
   end
 end
