ab494cf9c517f1b5d104d726dd95c5bedb555b87
Rogelio Zamudio <yeyozaz@gmail.com>
2017-09-30 12:34:03 -0500

    refactored Board to reflect 2d array, refactored tests to reflect changes

--- lib/board.rb
+++ lib/board.rb
@@ -1,27 +1,47 @@
 class Board
-  attr_reader :board
+  attr_reader :squares
 
   # initializes an instance of the board
   def initialize
-    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @squares = [
+      [nil, nil, nil],
+      [nil, nil, nil],
+      [nil, nil, nil],
+    ]
+  end
+
+  def display_square(row, column)
+    (row * 3) + column + 1
   end
 
   # used to print the board
+  def board_string
+    [
+      "#{display_square(0, 0)} | #{display_square(0, 1)} | #{display_square(0, 2)} ",
+      "#{display_square(1, 0)} | #{display_square(1, 1)} | #{display_square(1, 2)} ",
+      "#{display_square(2, 0)} | #{display_square(2, 1)} | #{display_square(2, 2)} "
+    ].join("\n")
+  end
+
   def print_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    puts board_string
   end
 
   # changes the value of the array index to the players mark
   def make_move(position, player)
-    @board.delete_at(position)
-    @board.insert(position, player)
+    row, col = get_index(position: position)
+    if @squares[row][col].nil?
+      @squares[row][col] = player
+    else
+      raise ArgumentError.new("Position already taken!")
+    end
   end
 
   # returns the value at the index provided
-  def get_index(position)
-    @board[position]
+  def get_index(position:)
+    row = (position - 1) / 3
+    col = (position - 1) % 3
+    return row, col
   end
 
   def diagonals
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,92 +1,120 @@
 require 'board'
 
-describe Board
+describe Board do
+  context '#initialize' do
+    it 'initializes the game board as a 2D array' do
+      game = Board.new
+      expect(game.squares).to eq(
+       [
+        [nil, nil, nil],
+        [nil, nil, nil],
+        [nil, nil, nil],
+       ]
+      )
+    end
+  end
 
-  describe 'the tic tac toe board' do
-    context '#initialize' do
-      it 'initializes the game board as an array' do
+  describe '#board_string' do
+    context 'when the board is empty' do
+      it 'prints the board sauare numbers' do
         game = Board.new
-        expect(game.board).to be_instance_of(Array)
-      end
-
-      it 'returns an array of numbers including 1 - 9' do
-        board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        game = Board.new
-        game.board
-        expect(game.board).to eq(board)
+        expect(game.board_string).to match(" 1 | 2 | 3 \n 4 | 5 | 6 \n 7 | 8 | 9 ")
       end
     end
 
-    context '#print_board' do
-      it 'prints the board' do
-        game = Board.new
-        expect((puts " 1 | 2 | 3 \n 4 | 5 | 6 \n 7 | 8 | 9 ")).to match(game.print_board)
-      end
+    context "when the board has squares filled" do
+      it 'prints the board square number where empty, and markers where occupied'
+    end
+  end
 
-      it 'prints the game to stdout' do
+  describe '#print_board' do
+    it 'prints the game to stdout' do
+      game = Board.new
+      expect { game.print_board }.to output.to_stdout
+    end
+  end
+
+  describe '#get_index' do
+    it "returns the row and column given a position number" do
+      board = Board.new
+      expect(board.get_index(position: 1)).to eq([0, 0])
+      expect(board.get_index(position: 5)).to eq([1, 1])
+      expect(board.get_index(position: 9)).to eq([2, 2])
+    end
+  end
+
+  describe '#make_move' do
+    it 'replaces the original cell value with an X at index 0' do
+      game = Board.new
+      game.make_move(1, 'X')
+      board = [
+        ['X', nil, nil],
+        [nil, nil, nil],
+        [nil, nil, nil],
+      ]
+
+      expect(game.squares).to eq(board)
+    end
+
+    context "when square is already occupied" do
+      it "raises exception" do
         game = Board.new
-        expect { game.print_board }.to output.to_stdout
+        game.make_move(1, 'X')
+
+        expect { game.make_move(1, 'X') }.to raise_exception("Position already taken!")
       end
     end
 
-    context '#make_move' do
-      it 'replaces the original cell value with an X at index 0' do
-        game = Board.new
-        game.make_move(0, 'X')
-        board = ['X', 2, 3, 4, 5, 6, 7, 8, 9]
-        expect(game.board).to eq(board)
-      end
-
-      it 'replace the original cell value with an O at index 0' do
-        game = Board.new
-        game.make_move(0, 'O')
-        board = ['O', 2, 3, 4, 5, 6, 7, 8, 9]
-        expect(game.board).to eq(board)
-      end
-
-      it 'replaces the orignal cell value with an X at index 5' do
-        game = Board.new
-        game.make_move(5, 'X')
-        board = [1, 2, 3, 4, 5, 'X', 7, 8, 9]
-        expect(game.board).to eq(board)
-      end
-
-      it 'relace the original cell value with an O at index 5' do
-        game = Board.new
-        game.make_move(5, 'O')
-        board = [1, 2, 3, 4, 5, 'O', 7, 8, 9]
-        expect(game.board).to eq(board)
-      end
+    it 'replace the original cell value with an O at index 0' do
+      game = Board.new
+      game.make_move(0, 'O')
+      board = ['O', 2, 3, 4, 5, 6, 7, 8, 9]
+      expect(game.board).to eq(board)
     end
 
-    context '#get_index' do
-      it 'gets the index and returns the value' do
-        game = Board.new
-        expect(game.get_index(0)).to eq(1)
-        expect(game.get_index(1)).to eq(2)
-      end
+    it 'replaces the orignal cell value with an X at index 5' do
+      game = Board.new
+      game.make_move(5, 'X')
+      board = [1, 2, 3, 4, 5, 'X', 7, 8, 9]
+      expect(game.board).to eq(board)
     end
 
-    context '#diagonals' do
-      # this test needs to be rewritten
-      it 'checks the diagonals for winning sets' do
-        game = Board.new
-        game.make_move(0, 'X')
-        game.make_move(4, 'X')
-        game.make_move(8, 'X')
-        diagonal = [game.get_index(0), game.get_index(4), game.get_index(8)]
-        winning_diagonal = diagonal
-        player_diagonal = game.board.select { |pos| pos == 'X' }
-        expect(winning_diagonal).to eq(player_diagonal)
-      end
+    it 'relace the original cell value with an O at index 5' do
+      game = Board.new
+      game.make_move(5, 'O')
+      board = [1, 2, 3, 4, 5, 'O', 7, 8, 9]
+      expect(game.board).to eq(board)
+    end
+  end
 
-      it 'maintains a reference to the winning diagonals' do
-        game = Board.new
-        board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        expect(game.diagonals).to eq([
-                                       [board[0], board[4], board[8]],
-                                       [board[2], board[4], board[6]]
-                                     ])
+  context '#get_index' do
+    it 'gets the index and returns the value' do
+      game = Board.new
+      expect(game.get_index(0)).to eq(1)
+      expect(game.get_index(1)).to eq(2)
+    end
+  end
+
+  context '#diagonals' do
+    # this test needs to be rewritten
+    it 'checks the diagonals for winning sets' do
+      game = Board.new
+      game.make_move(0, 'X')
+      game.make_move(4, 'X')
+      game.make_move(8, 'X')
+      diagonal = [game.get_index(0), game.get_index(4), game.get_index(8)]
+      winning_diagonal = diagonal
+      player_diagonal = game.board.select { |pos| pos == 'X' }
+      expect(winning_diagonal).to eq(player_diagonal)
+    end
+
+    it 'maintains a reference to the winning diagonals' do
+      game = Board.new
+      board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+      expect(game.diagonals).to eq([
+        [board[0], board[4], board[8]],
+        [board[2], board[4], board[6]]
+        ])
       end
     end
 
@@ -95,48 +123,48 @@ describe Board
         game = Board.new
         board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         expect(game.horizontals).to eq([
-                                         [board[0], board[1], board[2]],
-                                         [board[3], board[4], board[5]],
-                                         [board[6], board[7], board[8]]
-                                       ])
+          [board[0], board[1], board[2]],
+          [board[3], board[4], board[5]],
+          [board[6], board[7], board[8]]
+          ])
+        end
       end
-    end
 
-    context '#verticals' do
-      it 'maintains a reference to the winning verticals' do
-        game = Board.new
-        board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        expect(game.verticals).to eq([
-                                       [board[0], board[3], board[6]],
-                                       [board[1], board[4], board[7]],
-                                       [board[2], board[5], board[8]]
-                                     ])
-      end
-    end
+      context '#verticals' do
+        it 'maintains a reference to the winning verticals' do
+          game = Board.new
+          board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+          expect(game.verticals).to eq([
+            [board[0], board[3], board[6]],
+            [board[1], board[4], board[7]],
+            [board[2], board[5], board[8]]
+            ])
+          end
+        end
 
-    context '#winning_positions' do
-      it 'collects all winning combinations' do
-        game = Board.new
-        board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        expect(game.winning_positions).to eq([
-                                               [board[0], board[4], board[8]],
-                                               [board[2], board[4], board[6]],
-                                               [board[0], board[1], board[2]],
-                                               [board[3], board[4], board[5]],
-                                               [board[6], board[7], board[8]],
-                                               [board[0], board[3], board[6]],
-                                               [board[1], board[4], board[7]],
-                                               [board[2], board[5], board[8]]
-                                             ])
-      end
-    end
+        context '#winning_positions' do
+          it 'collects all winning combinations' do
+            game = Board.new
+            board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            expect(game.winning_positions).to eq([
+              [board[0], board[4], board[8]],
+              [board[2], board[4], board[6]],
+              [board[0], board[1], board[2]],
+              [board[3], board[4], board[5]],
+              [board[6], board[7], board[8]],
+              [board[0], board[3], board[6]],
+              [board[1], board[4], board[7]],
+              [board[2], board[5], board[8]]
+              ])
+            end
+          end
 
-    # it 'checks for a winning set' do
-    #   game = Board.new
-    #   player = 'X'
-    #   game.make_move(1, player)
-    #   game.make_move(4, player)
-    #   game.make_move(7, player)
-    #   expect(game.winner?).to eq(true)
-    # end
-  end
+          # it 'checks for a winning set' do
+          #   game = Board.new
+          #   player = 'X'
+          #   game.make_move(1, player)
+          #   game.make_move(4, player)
+          #   game.make_move(7, player)
+          #   expect(game.winner?).to eq(true)
+          # end
+        end
